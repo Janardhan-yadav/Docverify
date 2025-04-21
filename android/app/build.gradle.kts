14301e2b2a124ea16e5b53983250d42f26303e6d
Janardhan-yadav <yadavjanardhan91@gmail.com>
2025-04-21 15:05:44 +0530

    Initial commit for DocVerify

---
+++ android/app/build.gradle.kts
@@ -0,0 +1,48 @@
+plugins {
+    id("com.android.application")
+    // START: FlutterFire Configuration
+    id("com.google.gms.google-services")
+    // END: FlutterFire Configuration
+    id("kotlin-android")
+    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
+    id("dev.flutter.flutter-gradle-plugin")
+    id("org.jetbrains.kotlin.android") 
+}
+
+android {
+    namespace = "com.example.docverify"
+    compileSdk = 34
+    ndkVersion = "29.0.13113456"
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_11.toString()
+    }
+
+    defaultConfig {
+        applicationId = "com.example.docverify"
+        minSdk = 23
+        targetSdk = flutter.targetSdkVersion
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
+    }
+
+    buildTypes {
+        release {
+            signingConfig = signingConfigs.getByName("debug")
+        }
+    }
+}
+
+flutter {
+    source = "../.."
+}
+
+dependencies {
+    implementation("com.google.firebase:firebase-auth:21.0.1")  // Correct Firebase Authentication dependency
+    // Other dependencies...
+}
